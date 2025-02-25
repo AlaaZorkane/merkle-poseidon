873953a9e35b2ba77eb25912383a8217d56ed672
Alaa Zorkane <alaazorkane@gmail.com>
2025-02-25 03:06:26 +0000

    feat: more errors

--- src/errors.rs
+++ src/errors.rs
@@ -13,10 +13,16 @@ pub enum PoseidonMerkleError {
     InvalidBitsPathHash,
     #[error("proof error: {0}")]
     SiblingNotFound(#[from] ProofError),
+    #[error("invalid level")]
+    InvalidLevel,
 }
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ProofError {
     #[error("sibling at position {0} not found to generate proof")]
     SiblingNotFound(usize),
+    #[error("encountered a leaf node where an inner node was expected")]
+    InnerNodeExpected,
+    #[error("empty node hash")]
+    EmptyNodeHash,
 }
